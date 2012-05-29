5b3da20eeeedf640b8770100540a2ce410cb1f98
David Egolf <pheasants24@gmail.com>
2012-05-29 16:39:30 -0400

    Initial Commit

---
+++ cppTD/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cppTD
+//
+//  Created by David Egolf on 5/29/12.
+//  Copyright (c) 2012 Kutztown University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
